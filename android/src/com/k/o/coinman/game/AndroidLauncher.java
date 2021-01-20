981d32006cf8cd0db75678d00961995636b9262e
Camillo-commits <58623609+Camillo-commits@users.noreply.github.com>
2021-01-20 19:20:28 +0100

    Add files via upload

---
+++ android/src/com/k/o/coinman/game/AndroidLauncher.java
@@ -0,0 +1,21 @@
+package com.k.o.coinman.game;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.k.o.coinman.game.CoinMan;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useGyroscope = true;
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		initialize(new CoinMan(), config);
+	}
+}
